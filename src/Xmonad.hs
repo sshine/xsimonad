326ea47aa46b3c69f97299c461535632dfdf5166
Simon Shine <shreddedglory@gmail.com>
2017-01-13 23:14:41 +0100

    Refine dzen2 (leave out layout name)

--- src/Xmonad.hs
+++ src/Xmonad.hs
@@ -10,18 +10,22 @@ import XMonad.Layout.LayoutHints (layoutHints)
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, PP(..))
 import XMonad.Hooks.ManageDocks -- (avoidStruts, manageDocks, ToggleStruts)
 
---windowKey :: KeyMask
+windowKey :: KeyMask
 windowKey = mod4Mask
+
+layout :: Handle -> PP
+layout handle = def { ppLayout = const ""
+                    , ppOutput = hPutStrLn handle }
 
 main :: IO ()
 main = do
-  dzenRight <- spawnPipe "i3status | dzen2 -e - -h 32 -w 860 -x 1700 -y 0 -ta r"
-  dzenLeft <- spawnPipe "dzen2 -h 32 -w 2000 -x 0 -y 0 -ta l"
+  spawn "i3status | dzen2 -e - -h 32 -w 1280 -x 1280 -y 0 -ta r"
+  dzenLeft <- spawnPipe "dzen2 -h 32 -w 1280 -x 0 -y 0 -ta l"
   xmonad $ def { modMask = windowKey
                , terminal = "termite"
                , manageHook = manageDocks <+> manageHook def
                , layoutHook = avoidStruts . layoutHints . smartBorders $ Full
-               , logHook = dynamicLogWithPP $ def { ppOutput = hPutStrLn dzenLeft }
+               , logHook = dynamicLogWithPP $ layout dzenLeft
                , startupHook = do
                    spawn "xbindkeys"
                    spawn "feh --bg-scale ~/hask-bg-1920x1200.png"
