170e8c4d68edf2fd3823461ca104e22d41210363
Simon Shine <shreddedglory@gmail.com>
2017-01-13 22:33:19 +0100

    Added i3status, dzen, volume control, termite font, background image

--- src/Xmonad.hs
+++ src/Xmonad.hs
@@ -1,7 +1,30 @@
 module Main where
 
+import System.IO
+
 import XMonad
-import Data.Default
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Layout.LayoutHints (layoutHints)
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, PP(..))
+import XMonad.Hooks.ManageDocks -- (avoidStruts, manageDocks, ToggleStruts)
+
+--windowKey :: KeyMask
+windowKey = mod4Mask
 
 main :: IO ()
-main = xmonad def
+main = do
+  dzenRight <- spawnPipe "i3status | dzen2 -e - -h 32 -w 860 -x 1700 -y 0 -ta r"
+  dzenLeft <- spawnPipe "dzen2 -h 32 -w 2000 -x 0 -y 0 -ta l"
+  xmonad $ def { modMask = windowKey
+               , terminal = "termite"
+               , manageHook = manageDocks <+> manageHook def
+               , layoutHook = avoidStruts . layoutHints . smartBorders $ Full
+               , logHook = dynamicLogWithPP $ def { ppOutput = hPutStrLn dzenLeft }
+               , startupHook = do
+                   spawn "xbindkeys"
+                   spawn "feh --bg-scale ~/hask-bg-1920x1200.png"
+             }
+    `additionalKeys`
+    [ ((windowKey, xK_b), sendMessage ToggleStruts) ]
